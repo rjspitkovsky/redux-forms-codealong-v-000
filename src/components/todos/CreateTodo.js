568a952a47c0a03caf002da6f802c6c9509266db
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-06-06 18:13:37 +0000

    automatically backed up by learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -4,10 +4,16 @@ class CreateTodo extends Component {
   render() {
     return(
       <div>
-        Create Todo Component
+        <form>
+          <p>
+            <label>add todo</label>
+            <input type="text" />
+          </p>
+          <input type="submit" />
+        </form>
       </div>
-    )
+    );
   }
-}
+};
 
 export default CreateTodo;
